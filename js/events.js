a84f85f7d9b532a13e4f8bbee77af87386250111
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-21 02:11:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,6 +10,12 @@ function frameIt() {
   })
 }
 
+function submitIt() {
+  $('form').on("submit", () => {
+    alert('Your form is going to be sumbitted now.');
+  }) 
+}
+
 $(document).ready(function(){
 
 // call functions here
