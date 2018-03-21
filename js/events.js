10a134eb25e95aed7cc59f48c5c52f8d22f430c4
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-21 02:18:15 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function submitIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', () => {
+  $('input').on('keydown', (key) => {
     if (key.which == 71) {
       alert('G was pressed');
     }
